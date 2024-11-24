512c5ddba003e621b47f1d94b168d989e876151b
bertrandOrlando <91016925+Frox8@users.noreply.github.com>
2024-11-24 20:12:19 +0700

    add: database configuration

---
+++ src/database/database.js
@@ -0,0 +1,13 @@
+import pkg from "pg";
+import { DB_CONFIG } from "../config/appConfig";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: DB_CONFIG.USER,
+  host: DB_CONFIG.HOST,
+  database: DB_CONFIG.DATABASE,
+  password: DB_CONFIG.PASSWORD,
+  port: DB_CONFIG.PORT,
+});
+
+export default pool;
